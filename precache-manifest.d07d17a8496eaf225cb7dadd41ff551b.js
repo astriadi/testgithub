fabccc9f3cf9b1f9cef75693ecde9b857989bdf6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-12 04:57:12 +0000

    Deploy astriadi/testgithub to github.com/astriadi/testgithub.git:gh-pages

---
+++ precache-manifest.d07d17a8496eaf225cb7dadd41ff551b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9c2bb276e53fd73f961b5d685e6ef63",
+    "url": "/testgithub/index.html"
+  },
+  {
+    "revision": "0b7507482582716f1fe9",
+    "url": "/testgithub/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/testgithub/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "0b7507482582716f1fe9",
+    "url": "/testgithub/static/js/main.e744c1c9.chunk.js"
+  },
+  {
+    "revision": "f6d32fd4abd4fbbe9763",
+    "url": "/testgithub/static/js/runtime~main.472059c6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testgithub/static/media/logo.5d5d9eef.svg"
+  }
+]);
